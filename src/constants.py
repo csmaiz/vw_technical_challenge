180132bd221c25a4f1ad5595e2f19b643f38e2e2
Cristina Sanchez Maiz <cristina.sanchez4@vodafone.com>
2024-08-11 11:23:39 +0200

    cleaned code and added constants

--- src/constants.py
+++ src/constants.py
@@ -13,4 +13,24 @@ NEUTRAL_LABEL = "neutral"
 
 
 
+# Directories and files 
 
+INPUT_DIR = "../input/"
+OUTPUT_DIR = "../output/"
+
+TASK1_DIR = "task1"
+TASK2_DIR = "task2"
+TASK3_DIR = "task3"
+TASK4_DIR = "task4"
+
+FIGURES_DIR = "figures"
+FILENAMES_DIR = "cleaned_files"
+
+
+TRANSACTIONS_FILENAME = "customer_transactions_with_errors.csv"
+TRANSACTIONS_CLEANED_FILENAME = "transactions_cleaned.csv"
+
+REVIEWS_FILENAME = "customer_reviews_with_errors.csv"
+REVIEWS_CLEANED_FILENAME = "reviews_wtih_scores.csv"
+
+LDA_VIS_FILENAME = "ldavis.html"
